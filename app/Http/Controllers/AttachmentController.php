16f7f78c215f963a2691c67e965e75cd6edfba9d
Marcel <kgabinmarcel@gmail.com>
2026-07-14 22:46:02 +0100

    ui

--- app/Http/Controllers/AttachmentController.php
+++ app/Http/Controllers/AttachmentController.php
@@ -64,7 +64,7 @@ class AttachmentController extends Controller
     public function store(StoreAttachmentRequest $request)
     {
         // Validation
-        //  return
+         return
         $validated = $request->validated();
 
         $user = $request->get("user");
