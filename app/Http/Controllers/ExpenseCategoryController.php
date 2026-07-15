763d6b76624d9f66748ab0664da3af4d83a244ce
Marcel <kgabinmarcel@gmail.com>
2026-07-15 12:49:59 +0100

    ui

--- app/Http/Controllers/ExpenseCategoryController.php
+++ app/Http/Controllers/ExpenseCategoryController.php
@@ -16,7 +16,7 @@ class ExpenseCategoryController extends Controller
     public function index()
     {
         $categories = ExpenseCategory::select('id', 'name')
-            ->whereHas('rule')
+            // ->whereHas('rule')
             ->orderBy('name')
             ->get();
 
