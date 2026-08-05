9060a6aeef51c163858f4d29a7feb792daaf7797
Marcel <kgabinmarcel@gmail.com>
2026-08-05 08:30:27 +0100

    ui

--- app/Http/Controllers/DocumentController.php
+++ app/Http/Controllers/DocumentController.php
@@ -324,7 +324,7 @@ class DocumentController extends Controller
         $participants = $data["participants"];
         $business_signatures = $data["business_signatures"];
 
-        throw new Exception(json_encode((collect($participants))), 1);
+        // throw new Exception(json_encode((collect($participants))), 1);
         // throw new Exception(json_encode((collect($participants)->pluck('source_value'))), 1);
         // throw new Exception(json_encode((collect($business_signatures))), 1);
 
