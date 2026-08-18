676de8eb54e295485093681930262f702fa8dffa
Marcel <kgabinmarcel@gmail.com>
2026-08-18 07:06:42 +0100

    ui

--- app/Models/TaxiPaper.php
+++ app/Models/TaxiPaper.php
@@ -34,45 +34,29 @@ class TaxiPaper extends Model implements PayableDocumentInterface
     {
         return $this->belongsTo(Document::class,);
     }
-    public function createSettlementRecord(
-        string $transactionTypeCode,
-        string $transactionCode
-    ): void {
+   public function createSettlementRecord(
+    string $transactionTypeCode,
+    string $transactionCode
+): void {
 
+    $direction = $this->getSettlementDirection($transactionTypeCode);
+    $amount = $this->getSettlementAmount($transactionTypeCode);
 
-            $direction = $this->getSettlementDirection($transactionTypeCode);
-            $amount = $this->getSettlementAmount($transactionTypeCode);
-
-            $this->financialTransactions()->create([
-    'transaction_code' => $transactionCode,
-    'transaction_type_code' => $transactionTypeCode,
-    'type' => 'ONE_SHOT',
-    'adjustment_type' => 'NONE',
-    'amount' => $amount,
-    'direction' => $direction,
-    'status' => 'PENDING',
-    // 'paid_at' => now(),
-    // 'processed_at' => now(),
-    'created_by' => request()->get('user')['id']
-]);
-           
-
-            // $this->regulations()->firstOrCreate(
-            //     [
-            //         'transaction_code' => $transactionCode,
-            //     ],
-            //     [
-            //         'amount' => $this->getSettlementAmount($transactionTypeCode),
-
-            //            'type' =>'supplement' ,// $direction === 'OUT' ? 'supplement'  : 'refund',
-
-            //         'status' => 'PENDING',
-            //     ]
-            // );
-
-          
-        
-    }
+    $this->financialTransactions()->firstOrCreate(
+        [
+            'transaction_code' => $transactionCode,
+        ],
+        [
+            'transaction_type_code' => $transactionTypeCode,
+            'type' => 'ONE_SHOT',
+            'adjustment_type' => 'NONE',
+            'amount' => $amount,
+            'direction' => $direction,
+            'status' => 'PENDING',
+            'created_by' => request()->get('user')['id'],
+        ]
+    );
+}
 
 
       public function financialTransactions()
